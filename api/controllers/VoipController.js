e3b9a6d20ea56a9aac21e3f3de0a7ae33da496ab
Aamir Latif <aamir@sellyx.com>
2017-02-18 22:56:54 -0800

    access token for voip

--- api/controllers/VoipController.js
+++ api/controllers/VoipController.js
@@ -23,6 +23,41 @@ module.exports = {
 
 		    process.stdout.write(call.sid);
 			});
+		},
+	token : function(req, res){
+		var AccessToken = require('twilio').AccessToken;
+		var IpMessagingGrant = AccessToken.VoiceGrant;
+
+		// Used when generating any kind of tokens
+		var twilioAccountSid = 'ACf2a6b1837b585b0a10259694beb74174';
+		var twilioApiKey = 'SK5778b44dc3d6981b051f6ac738a63164';
+		var twilioApiSecret = 'oXhmdigzMWkior13sHcAuEWrkR1FD17B';
+
+		// Used specifically for creating IP Messaging tokens
+		var serviceSid = 'APd815d05ada1d681bbbc8b1fe26bd196f';
+		var appName = 'Vendr Inc.';
+		var identity = 'aamir@vendr.tech';
+		var deviceId = 'someiosdeviceid';
+		var endpointId = appName + ':' + identity + ':' + deviceId;
+
+		// Create a "grant" which enables a client to use IPM as a given user,
+		// on a given device
+		var ipmGrant = new VoiceGrant({
+		    serviceSid: serviceSid,
+		    endpointId: endpointId
+			});
+
+		// Create an access token which we will sign and return to the client,
+		// containing the grant we just created
+		var token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
+		token.addGrant(ipmGrant);
+		token.identity = identity;
+
+		// Serialize the token to a JWT string
+		console.log(token.toJwt());
+
+
+		return res.send(200,Response.success(token.toJwt()))
 		}
 
 	};
