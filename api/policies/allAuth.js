48db5c2d4f015aa3810df3e570e6d8af1b55f784
Aamir Latif <aamir@sellyx.com>
2017-02-19 03:29:24 -0800

    changes

--- api/policies/allAuth.js
+++ api/policies/allAuth.js
@@ -1,11 +1,7 @@
 
 module.exports = function(req, res, next) {
 
-	if(!req.headers['fb_at']) return res.forbidden(Response.failure("This call requires a valid Facebook token."))
-
-	req.fb_at = req.headers['fb_at']
-	
-	fb.setAccessToken(req.fb_at)
+	console.log("all auth.")
 
 	return next();
 	};
