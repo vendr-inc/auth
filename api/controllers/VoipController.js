2730b273322e0dd96b98519f0532b7fbfb21936e
Aamir Latif <aamir@sellyx.com>
2017-02-20 13:36:37 -0800

    updates

--- api/controllers/VoipController.js
+++ api/controllers/VoipController.js
@@ -49,7 +49,7 @@ module.exports = {
 		var twiml = new twilio.TwimlResponse();
 		twiml.say("Welcome to Vendr! We are connecting your call.");
 
-		twiml.dial("+18054037831")
+		twiml.dial("+18054037831", {callerId: "+13103128690" })
 
 		res.setHeader("content-type", "text/xml")
 
