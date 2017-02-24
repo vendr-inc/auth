0a112c8c3fda0c91ea66fd5582d79f2c7f2c407a
Aamir Latif <aamir@sellyx.com>
2017-02-24 12:11:30 -0800

    connect and disconnect

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -9,28 +9,62 @@ module.exports = {
 	
 	connect : function(req, res){
 
-		console.log(req.active_account.id)
-		console.log(sails.sockets)
+		var data = {
+			to : { v:'string' }
+			}
+		
+		data = Validator.run(data, req.body)
+		if(data.failure) return res.send(200,data)
 
-		sails.sockets.blast('message', { "message"  : req.body.message });
+
+		co(function*(){
+
+			var to_account = yield Accounts.findOne({ id: data.to  })
+			if(!to_account) return res.send(200, Response.failure("No account found"))
+
+			var room = (((to_account.id).localCompare(req.active_account.id) == -1) ? to_account.id + "_" + req.active_account.id : req.active_account.id + "_" + to_account.id)
+
+		console.log(room)
+
+			
+			sails.sockets.join(sails.sockets.getId() , room, function(err){
+
+				if(err) return res.send(200, Response.failure(err))
+
+				})
+	
+			}).catch(err => res.send(200,Response.failure(err)))
 
 
 		},
-	subscribeToFunRoom: function(req, res) {
-		// if (!req.isSocket) {
-		// 	return res.badRequest();
-		// 	}
+	disconnect : function(req, res){
 
-		// var roomName = req.param('roomName');
-		// 	sails.sockets.join(req, roomName, function(err) {
-		// 	if (err) {
-		// 	return res.serverError(err);
-		// 	}
+		var data = {
+			to : { v:'string' }
+			}
+		
+		data = Validator.run(data, req.body)
+		if(data.failure) return res.send(200,data)
 
-		// return res.json({
-		// 	message: 'Subscribed to a fun room called '+roomName+'!'
-		// 	});
-		// 	});
+
+		co(function*(){
+
+			var to_account = yield Accounts.findOne({ id: data.to  })
+			if(!to_account) return res.send(200, Response.failure("No account found"))
+		
+			var room = (((to_account.id).localCompare(req.active_account.id) == -1) ? to_account.id + "_" + req.active_account.id : req.active_account.id + "_" + to_account.id)
+
+		console.log(room)
+			
+			sails.sockets.leave(sails.sockets.getId() , room , function(err){
+
+				if(err) return res.send(200, Response.failure(err))
+
+				})
+	
+			}).catch(err => res.send(200,Response.failure(err)))
+
+
 		}
 
 	};
