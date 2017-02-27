df5d25c0bfe996a613afb7032d5bec8c7c899292
Aamir Latif <aamir@sellyx.com>
2017-02-26 23:14:10 -0800

    updates

--- api/controllers/VoipController.js
+++ api/controllers/VoipController.js
@@ -7,40 +7,40 @@
 
 module.exports = {
 	
-	outgoing : function(req,res){
+	// outgoing : function(req,res){
 
-		console.log("outgoing calls being connected")
+	// 	console.log("outgoing calls being connected")
 
-		var data = {
-			phone : { v:'phone' },
-			}
+	// 	var data = {
+	// 		phone : { v:'phone' },
+	// 		}
 
-		data = Validator.run(data,req.body);
-		if(data.failure) return res.send(200, data);
+	// 	data = Validator.run(data,req.body);
+	// 	if(data.failure) return res.send(200, data);
 
 
-		var client = require('twilio')("ACf2a6b1837b585b0a10259694beb74174", "365aa491eda9c6e67ccf897400b32bc6")
+	// 	var client = require('twilio')("ACf2a6b1837b585b0a10259694beb74174", "365aa491eda9c6e67ccf897400b32bc6")
 	
-		client.calls.create({
-		    url: "http://demo.twilio.com/docs/voice.xml",
-		    to: "+" + data.phone,
-		    from: "+13103128690"
-			}, 
+	// 	client.calls.create({
+	// 	    url: "http://demo.twilio.com/docs/voice.xml",
+	// 	    to: "+" + data.phone,
+	// 	    from: "+13103128690"
+	// 		}, 
 
-		function(err, call) {
+	// 	function(err, call) {
 
-			if(err) return res.send(200, Response.failure("Outgoing number not valid."))
+	// 		if(err) return res.send(200, Response.failure("Outgoing number not valid."))
 
-			return res.send(200, Response.success("Call placed"))
+	// 		return res.send(200, Response.success("Call placed"))
 
-			console.log(err)
-			console.log(call)
+	// 		console.log(err)
+	// 		console.log(call)
 
 
-		    process.stdout.write(call.sid);
-			});
-		},
-	incoming : function(req, res){
+	// 	    process.stdout.write(call.sid);
+	// 		});
+	// 	},
+	outgoing : function(req, res){
 
 		console.log("here")
 
@@ -74,7 +74,8 @@ module.exports = {
 		// Create a "grant" which enables a client to use IPM as a given user,
 		// on a given device
 		var ipmGrant = new vgrant({
-		    outgoingApplicationSid: serviceSid
+		    outgoingApplicationSid: serviceSid,
+		    pushCredentialSid : 'CR90a7f722c817fddaa1b77b554dc657d8'
 			});
 
 		// Create an access token which we will sign and return to the client,
