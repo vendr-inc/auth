51b9692921fe48554948cd1a32680b09cbfb9813
Aamir Latif <aamir@sellyx.com>
2017-03-17 00:04:42 -0700

    need to see header data

--- api/policies/facebookAuth.js
+++ api/policies/facebookAuth.js
@@ -1,6 +1,10 @@
 
 module.exports = function(req, res, next) {
 
+
+	console.log(req.headers)
+	console.log(req.body)
+
 	if(!req.headers['fb_at']) return res.forbidden(Response.failure("This call requires a valid Facebook token."))
 
 	req.fb_at = req.headers['fb_at']
