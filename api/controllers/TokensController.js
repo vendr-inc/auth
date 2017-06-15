7edc6aa91a4cfd658b16c7d0d944fcc26d1d01ae
Aamir Latif <aamir@sellyx.com>
2017-06-14 18:10:08 -0700

    testing verification

--- api/controllers/TokensController.js
+++ api/controllers/TokensController.js
@@ -16,51 +16,71 @@ module.exports = {
 
 		Accounts.findOne({phone:data.phone}).exec(function(err,found){
 
-			console.log(err)
-			console.log(found)
-
 			if(err) return res.send(200, Response.failure(err))
-
-
 			if(found) return res.send(200, Response.failure("This phone number has already been registered."))
 
-			Tokens.findOne({ data : data.phone , type : "phone/send" }).exec(function(err, found){
-				if(err) return res.send(200, Response.failure(err))
+			// verify that number is real
+
+			var r = require('request')
+			var request = r.post({
+				url : 'https://api.authy.com/protected/json/phones/verification/start?api_key=JTPtfmOKep0j5Z5Rnt70y1wXT8DeE6WL', 
+				headers : {"Content-Type":"application/x-www-form-urlencoded"},
+				form : {  phone_number : data.phone, country_code : 1, via : 'sms'  }
+				} , function(err, resp, body){
+
+					console.log(err)
+					console.log(resp)
+					console.log(body)
+
+					co(function*(){
+						
+
+
+
+
+						}).catch(err => res.send(200,Response.failure(err)))
+
+					})
+
+
+
+			// Tokens.findOne({ data : data.phone , type : "phone/send" }).exec(function(err, found){
+			// 	if(err) return res.send(200, Response.failure(err))
 				
-				var token = Math.floor(Math.random() * 9000) + 1000
+			// 	var token = Math.floor(Math.random() * 9000) + 1000
 
-				var client = require('twilio')("ACf2a6b1837b585b0a10259694beb74174", "365aa491eda9c6e67ccf897400b32bc6")
+			// 	var client = require('twilio')("ACf2a6b1837b585b0a10259694beb74174", "365aa491eda9c6e67ccf897400b32bc6")
 
-				client.messages.create({
-					to : data.phone,
-					from : "+13103128690",
-					body : "Your Vendr code is: " + token + ". Please use this code within 5 minutes to register your account successfully."
-					} , function(err, msg){
-						console.log(err)
-						if(err) return res.send(200, Response.failure("The code could not be sent."))
+			// 	client.messages.create({
+			// 		to : data.phone,
+			// 		from : "+13103128690",
+			// 		body : "Your Vendr code is: " + token + ". Please use this code within 5 minutes to register your account successfully."
+			// 		} , function(err, msg){
+			// 			console.log(err)
+			// 			if(err) return res.send(200, Response.failure("The code could not be sent."))
 
-						if(found){
-							Tokens.update({data:data.phone,type:"phone/send"},{
-								exp_time : Date.now() + (60*60*5*1000),
-								token : token,
-								}).exec(function(err, created){
-									if(err) return res.send(200, Response.failure(err))
-									return res.send(200, Response.success("A code was sent to " + data.phone + "."))
-									})
-							}
-						else{
-							Tokens.create({
-								type : "phone/send",
-								token : token,
-								data : data.phone,
-								exp_time : Date.now() + (60*60*5*1000),
-								}).exec(function(err, created){
-									if(err) return res.send(200, Response.failure(err))
-									return res.send(200, Response.success("A code was sent to " + data.phone + "."))
-									})
-							}
-						})
-				})
+			// 			if(found){
+			// 				Tokens.update({data:data.phone,type:"phone/send"},{
+			// 					exp_time : Date.now() + (60*60*5*1000),
+			// 					token : token,
+			// 					}).exec(function(err, created){
+			// 						if(err) return res.send(200, Response.failure(err))
+			// 						return res.send(200, Response.success("A code was sent to " + data.phone + "."))
+			// 						})
+			// 				}
+			// 			else{
+			// 				Tokens.create({
+			// 					type : "phone/send",
+			// 					token : token,
+			// 					data : data.phone,
+			// 					exp_time : Date.now() + (60*60*5*1000),
+			// 					}).exec(function(err, created){
+			// 						if(err) return res.send(200, Response.failure(err))
+			// 						return res.send(200, Response.success("A code was sent to " + data.phone + "."))
+			// 						})
+			// 				}
+			// 			})
+			// 	})
 			})
 		},
 	"phone/forgot" : function(req, res){
